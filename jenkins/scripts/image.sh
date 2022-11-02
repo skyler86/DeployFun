5cbfe7ce99eaa1f20d0c5b1ea6f160ef1c8bf853
skyler86 <xiaoyao_shaw@126.com>
2022-11-02 20:59:51 +0800

    skyler

--- jenkins/scripts/image.sh
+++ jenkins/scripts/image.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
- 
+
 set -x
  
 IMAGE_NAME="prometheus-test-demo"
 VERSION_ID="${BUILD_ID}"
-IMAGE_ADDR="test-harbor.daocloud.io/library/${IMAGE_NAME}"
+IMAGE_ADDR="192.168.2.10/jenkins/${IMAGE_NAME}"
  
 docker build -f Dockerfile --build-arg jar_name=target/prometheus-test-demo-0.0.1-SNAPSHOT.jar -t ${IMAGE_NAME}:${VERSION_ID} .
  
 docker tag  ${IMAGE_NAME}:${VERSION_ID}  ${IMAGE_ADDR}:${VERSION_ID}
-docker login --username=admin --password=Harbor12345 test-harbor.daocloud.io
+docker login --username=admin --password=rainbow123 192.168.2.10
 docker push ${IMAGE_ADDR}:${VERSION_ID}
