444dbe4acd9aeb9ce1dbd5976c44a98d454e4e68
skyler86 <xiaoyao_shaw@126.com>
2022-11-12 16:12:45 +0800

    skyler

---
+++ image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+IMAGE_NAME="prometheus-test-demo"
+VERSION_ID="${BUILD_ID}"
+IMAGE_ADDR="192.168.2.10/jenkins/${IMAGE_NAME}"
+
+docker build -f Dockerfile -t ${IMAGE_NAME}:${VERSION_ID} .
+
+docker tag  ${IMAGE_NAME}:${VERSION_ID}  ${IMAGE_ADDR}:${VERSION_ID}
+docker login --username=admin --password=rainbow123 192.168.2.10
+docker push ${IMAGE_ADDR}:${VERSION_ID}
