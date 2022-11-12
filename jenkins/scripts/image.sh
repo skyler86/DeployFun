513d10efd644b4beae4d6787e1b7b2cc36b9d09d
skyler86 <xiaoyao_shaw@126.com>
2022-11-12 15:45:51 +0800

    skyler

--- jenkins/scripts/image.sh
+++ jenkins/scripts/image.sh
@@ -4,7 +4,7 @@ IMAGE_NAME="prometheus-test-demo"
 VERSION_ID="${BUILD_ID}"
 IMAGE_ADDR="192.168.2.10/jenkins/${IMAGE_NAME}"
 
-docker build -f /var/jenkins_home/workspace/job-demo/Dockerfile -t ${IMAGE_NAME}:${VERSION_ID} .
+docker build -f /volume/nfs/jenkins/workspace/job-demo/Dockerfile -t ${IMAGE_NAME}:${VERSION_ID} .
 
 docker tag  ${IMAGE_NAME}:${VERSION_ID}  ${IMAGE_ADDR}:${VERSION_ID}
 docker login --username=admin --password=rainbow123 192.168.2.10
