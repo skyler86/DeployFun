ada5ab58e7e66daacab67d9f92f0d22b0d2a823b
skyler86 <xiaoyao_shaw@126.com>
2022-11-13 09:29:32 +0800

    skyler

--- image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION_ID=22
-
-docker build -t prometheus-test-demo:${VERSION_ID} -f Dockerfile .
-
-docker tag  prometheus-test-demo:${VERSION_ID} 192.168.2.10/jenkins/prometheus-test-demo:${VERSION_ID}
-docker login --username=admin --password=rainbow123 192.168.2.10
-docker push 192.168.2.10/jenkins/prometheus-test-demo:${VERSION_ID}
